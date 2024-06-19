4579f4a03a0685afc064cbf9099e50f4b5fbca73
MatykinaAnna <78198012+MatykinaAnna@users.noreply.github.com>
2024-06-19 11:52:37 +0300

    Update thunks.ts

--- src/features/Loader/Period/model/thunks.ts
+++ src/features/Loader/Period/model/thunks.ts
@@ -6,7 +6,7 @@ const loadPeriod = createAsyncThunk(
   async (payload: { company_group_id: number; price_type_id: number }) => {
     const listPeriod = await apiInstance({
       method: 'get',
-      url: `/api/v1/price_list/periods/?company_group_id=${payload.company_group_id}&price_type_id=${payload.price_type_id}`,
+      url: `/api/v1/price_list/periods?company_group_id=${payload.company_group_id}&price_type_id=${payload.price_type_id}`,
     }).then((res) => {
       return res.data
     })
